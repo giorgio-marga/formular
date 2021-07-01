a10588a909c7f6dad2801ef3ada12b8303764719
Giorgio Margarone <giorgio@MacBook-Pro.local>
2021-07-01 02:56:07 +0200

    add feature valid invalid

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("test erfolgt");
+
+$.fn.checkInvalid = function() {
+    if( $(this).is(':invalid') ) {
+        $(this).addClass('input-textarea-invalid');
+    }
+};
+
+$('form').on('focusout', '#input-name', function() {
+    $('#input-name').checkInvalid();
+});
+
+$('form').on('focusout', '#input-email', function() {
+    $('#input-email').checkInvalid();
+});
+
+$('form ').on('focusout', '#textarea-msg', function() {
+    $('#textarea-msg').checkInvalid();
+});
